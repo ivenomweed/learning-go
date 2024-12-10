c0cf5948b3b1d1493e0dd7b8da2a1dda65133e3b
VenomWeed <ivenomweed+github@gmail.com>
2024-12-10 11:11:33 +0530

    feat: 07-switch
    - Basic `switch` example

--- main.go
+++ main.go
@@ -1,3 +1,43 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	// Basic 'switch' statement
+	i := 2
+	fmt.Print("Write ", i, " as ")
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	}
+
+	// multiple comma seperated statements can be used as expressions for case
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("It's the weekend")
+	default:
+		fmt.Println("It's a weekday")
+	}
+
+	// A type switch can compare types instead of values
+	whatAmI := func(i interface{}) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I'm a bool")
+		case int:
+			fmt.Println("I'm an int")
+		default:
+			fmt.Printf("Don't know type %T\n", t)
+		}
+	}
+	whatAmI(true)
+	whatAmI(1)
+	whatAmI("hey")
+}
