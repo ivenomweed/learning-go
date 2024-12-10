e3c8b2f7ac8a042a570a8981bbc51bd4d3cc2740
VenomWeed <ivenomweed+github@gmail.com>
2024-12-10 10:52:53 +0530

    feat: 06-if-else
    - basic if/else example

--- main.go
+++ main.go
@@ -1,3 +1,14 @@
 package main
 
-func main() {}
+import "fmt"
+
+// If/Else
+
+func main() {
+	// Basic if/else example
+	if 7%2 == 0 {
+		fmt.Println("7 is even")
+	} else {
+		fmt.Println("7 is odd")
+	}
+}
