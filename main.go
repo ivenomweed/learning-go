dd60aceb165dd3168cf6a81e9442038e375c0332
VenomWeed <ivenomweed+github@gmail.com>
2024-12-10 08:07:41 +0530

    fix(main.go): Update Go code with basic types and operators.
    - Update the `main.go` file to include basic types and operators in Go programming, demonstrating string concatenation, basic arithmetic, boolean values, and logical operators.

--- main.go
+++ main.go
@@ -1,3 +1,21 @@
 package main
 
-func main() {}
+import "fmt"
+
+// Values: Go's types
+// Strings, Integers, Floats, Booleans
+
+func main() {
+	// String
+	fmt.Println("Hello" + "World")
+	// Integers
+	fmt.Println(1 + 1)
+	// Float
+	fmt.Println(7.0 / 3.0)
+	// Booleans
+	fmt.Println(true)
+	// Operators
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
