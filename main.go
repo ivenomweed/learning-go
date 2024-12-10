15c5757121afc51f5d565159e5b5f05af9ddda91
VenomWeed <ivenomweed+github@gmail.com>
2024-12-10 17:20:30 +0530

    feat: 09-10-functions
    - went over single return, multi return, variadic, closure and recursive functions

--- main.go
+++ main.go
@@ -1,3 +1,83 @@
 package main
 
-func main() {}
+import "fmt"
+
+// Functions
+
+// function to add two integers
+func plus(a int, b int) int {
+	return a + b
+}
+
+// function to add three integers
+func plusPlus(a, b, c int) int {
+	return a + b + c
+}
+
+// function that returns multiple values
+func vals() (int, int) {
+	return 3, 7
+}
+
+// variadic function
+func sum(nums ...int) {
+	fmt.Print(nums, "")
+	total := 0
+
+	for _, num := range nums {
+		total += num
+	}
+	fmt.Println(total)
+}
+
+// closures with annonymous fucntions
+func intSeq() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
+}
+
+// recursive functions
+func fact(n int) int {
+	if n == 0 {
+		return 1
+	}
+	fmt.Println(n)
+	return n * fact(n-1)
+}
+
+func main() {
+	fmt.Println(plus(1, 2))
+	fmt.Println(plusPlus(1, 2, 3))
+	a, b := vals()
+	fmt.Println(a)
+	fmt.Println(b)
+
+	_, c := vals()
+	fmt.Println(c)
+
+	sum(1, 2, 3, 4, 5)
+	nums := []int{1, 2, 3, 4}
+	sum(nums...)
+	nextInt := intSeq()
+
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+
+	newInts := intSeq()
+	fmt.Println(newInts())
+
+	fmt.Println(fact(7))
+	var fib func(n int) int
+
+	fib = func(n int) int {
+		if n < 2 {
+			return n
+		}
+		return fib(n-1) + fib(n-2)
+	}
+	fmt.Println(fib(7))
+}
