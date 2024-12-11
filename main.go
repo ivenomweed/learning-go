699ba577d32fde1efa8981803734ab75953382e1
VenomWeed <ivenomweed+github@gmail.com>
2024-12-11 14:25:22 +0530

    12-pointers

--- main.go
+++ main.go
@@ -1,3 +1,30 @@
 package main
 
-func main() {}
+import "fmt"
+
+// Pointers
+func zeroVal(ival int) {
+	ival = 0
+}
+func zeroPtr(iptr *int) {
+	*iptr = 0
+}
+
+func main() {
+	i := 1
+	fmt.Println("initial:", i)
+
+	// value of i doesn't change
+	zeroVal(i)
+	fmt.Println("zeroval:", i)
+
+	// value of i changes because the pointer to the memory address
+	//  is being passed instead of a
+	// reference so when zeroPtr changes the value it changes the value
+	// in the memory address directly
+	zeroPtr(&i)
+	fmt.Println("zeroptr:", i)
+
+	// Memory address
+	fmt.Println("pointer", &i)
+}
